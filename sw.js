100cd5e1e9601e89580a76bc9f6cd791f4a7e7d0
iamAmitkumar <pamit2897@gmail.com>
2020-01-12 15:14:19 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,71 @@
+
+const cacheName = 'site-pwa-cache';
+const assets = [
+    '/',
+    'index.html',
+    'index.css',
+    'js/app.js',
+    'images/avatar.png',
+    'https://fonts.googleapis.com/css?family=Dosis:400,500,600,700&display=swap'
+]
+
+// install service worker
+self.addEventListener('install', evt => {
+
+    evt.waitUntil(caches.open(cacheName).then(cache => {
+        console.log('caching shell assets')
+        cache.addAll(assets)
+        .catch(err => {
+            console.log('Error adding files to cache', err);
+        })
+    })
+  ); 
+  console.log('service worker has been installed');
+  self.skipWaiting(); 
+ 
+});
+
+// activate service worker
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+});
+
+// fetch event 
+self.addEventListener('fetch', evt => {
+   // console.log('fetch event', evt)
+   evt.respondWith(
+       caches.match(evt.request).then(cacheRes => {
+           return cacheRes || fetch(evt.request);
+       })
+   )
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
